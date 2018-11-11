7f29ce4a15307290b264c75c0da7c515c2514ab0
Ivan Rubinson <soryy708@gmail.com>
2018-11-11 12:10:38 +0200

    Fix leaks & corruption

--- Source Code/Ticket Management System/menu_option.h
+++ Source Code/Ticket Management System/menu_option.h
@@ -7,6 +7,5 @@ struct MenuOption;
 struct MenuOption* menuOption_create(wchar_t* title, void(*action)());
 void menuOption_destroy(struct MenuOption*);
 wchar_t* menuOption_getTitle(struct MenuOption*);
-void menuOption_setTitleLiteral(struct MenuOption*, wchar_t*);
-void menuOption_setTitleAlloc(struct MenuOption*, wchar_t*);
+void menuOption_setTitle(struct MenuOption*, wchar_t*);
 void menuOption_performAction(struct MenuOption*);
