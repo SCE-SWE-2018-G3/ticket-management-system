38b2159142e0c3037292a413db27bbd0b332e07d
Ivan Rubinson <soryy708@gmail.com>
2018-12-02 20:17:00 +0200

    Add documentation to headers

--- Source Code/Ticket Management System/auth.h
+++ Source Code/Ticket Management System/auth.h
@@ -3,8 +3,18 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// Attempts to log-in with the provided credentials.
+// Test for success by calling `auth_isAuthenticated()`.
 void auth_authenticate(char* email, wchar_t* password);
+
+// Logs out of the system
 void auth_logOut();
+
+// Are we logged in successfully?
 bool auth_isAuthenticated();
+
+// If we're logged in, are we a support giver?
 bool auth_isSupportGiver();
+
+// Gets the email of the logged in user
 char* auth_getEmail();
--- Source Code/Ticket Management System/menu_option.h
+++ Source Code/Ticket Management System/menu_option.h
@@ -4,8 +4,14 @@
 
 struct MenuOption;
 
+// Creates a new menu option. `action` will be called when it's selected.
 struct MenuOption* menuOption_create(wchar_t* title, void(*action)());
+
+// Destroys a menu option.
 void menuOption_destroy(struct MenuOption*);
+
+// Calls the `action` that was passed to `menuOption_create`.
+void menuOption_performAction(struct MenuOption*);
+
 wchar_t* menuOption_getTitle(struct MenuOption*);
 void menuOption_setTitle(struct MenuOption*, wchar_t*);
-void menuOption_performAction(struct MenuOption*);
